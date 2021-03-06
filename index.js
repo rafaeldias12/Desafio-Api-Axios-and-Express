6891a5de9d73d73bba2ad432557a583817a2890e
Rafael Dias <74469120+rafaeldias12@users.noreply.github.com>
2021-03-06 19:50:19 -0300

    Commit Express
    Incluído no mesmo index um Desafio da DH com Express

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const axios = require('axios');
 const express = require('express')
 const app = express()
 
+const pokemons = [
+    'Charizard',
+    'Pikachu',
+    'Piteco',
+    'Squirtle',
+];
+//Lsitar Pokemons no Brownser
+app.get('/pokemon', (req, res) => {
+    const { nome } = req.query;
+    let listaRetorno = pokemons.filter(i => i.includes(nome || ''))
+    return res.json(listaRetorno)
+ });
+
+//Consumindo API com Axios
 app.get('/pokemons', (req, res) => {
     let arrayPokemon = [];
     axios.get('https://pokeapi.co/api/v2/pokemon?offset=100&limit=5').then((respo) => {
